09ccd87db7a2a5ee5d7ed18361b2fb2fc28f0376
Hemi <leibo.hemi@gmail.com>
2020-09-01 12:39:55 +0300

    aaa

--- WebApplication24/Startup.cs
+++ WebApplication24/Startup.cs
@@ -50,7 +50,7 @@ namespace WebApplication24
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home4}/{action=Index}/{id?}");
+                    pattern: "{controller=Home5}/{action=Index}/{id?}");
             });
         }
     }
